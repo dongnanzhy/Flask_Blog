cf712efd6090516aa7148902d5f70eecdb4fd7e6
Yan Zhao <dongnanzhy@gmail.com>
2019-04-24 22:19:39 -0700

    1-getting started, 2-Templates, 3-Forms

---
+++ flaskblog.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
+
+# set secret key
+# use python: secrets.token_hex() to generate
+app.config['SECRET_KEY'] = '7532f6e85de5608e4051662da59e14005bb5f6f6274f87554dea9f523545ef65'
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # flash alert is integrated in layout.html
+        flash(f'Account created for {form.username.data}!', 'success')
+        # NOTE: url_for function's argument is name of route function!
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)

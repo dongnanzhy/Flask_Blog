dff55f34df337727c3167d06d46198a802176168
Yan Zhao <dongnanzhy@gmail.com>
2019-04-26 21:32:10 -0700

    7 - user account and profile picture

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -34,3 +36,29 @@ class LoginForm(FlaskForm):
   password = PasswordField('Password', validators=[DataRequired()])
   remember = BooleanField('Remember Me')
   submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+  # with form validation
+  username = StringField('Username',
+                          validators=[DataRequired(), Length(min=2, max=20)])
+  email = StringField('Email', 
+                      validators=[DataRequired(), Email()])
+  submit = SubmitField('Update')
+  picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+
+  ## Customized validation of form
+  def validate_username(self, username):
+    ## check whether input username already in database, 
+    ## only if username not equals to previous username
+    if username.data != current_user.username:
+      user = User.query.filter_by(username=username.data).first()
+      if user:
+        raise ValidationError('That username is taken. Please choose a different one.')
+
+  def validate_email(self, email):
+    ## check whether input email already in database, 
+    ## only if email not equals to previous email
+    if email.data != current_user.email:
+      user = User.query.filter_by(email=email.data).first()
+      if user:
+        raise ValidationError('That email is taken. Please choose a different one.')

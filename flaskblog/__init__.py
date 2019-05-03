745c1543c0c26157c8b934af23e9ff37e3d78f9c
Yan Zhao <dongnanzhy@gmail.com>
2019-05-02 21:33:14 -0700

    10-username and password reset

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask
 # used for sqlite
 from flask_sqlalchemy import SQLAlchemy
 # used for user authentication
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 ## set secret key
@@ -30,6 +32,14 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 ## bootsrap class = info
 login_manager.login_message_category = 'info'
+
+## auto-email
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 ## import here to deal with circular import issue
 from flaskblog import routes
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from flaskblog import db, login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flaskblog import db, login_manager, app
 from flask_login import UserMixin
 
 # login module
@@ -18,6 +19,20 @@ class User(db.Model, UserMixin):
     ## This is not to add a column to Post, just running a select query at runtime
     ## Adding "backref", can run "$ post.author" to get entire User instance
     posts = db.relationship('Post', backref='author', lazy=True)
+
+    ## reset user functions
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+    
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
